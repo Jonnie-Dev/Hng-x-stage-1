4d87dc5b760da032c74d77a84dd20b385954bbfb
Jonnie <jtech2096@gmail.com>
2023-09-08 14:57:43 +0100

    feat: add realtime date and time functionality

--- index.js
+++ index.js
@@ -29,3 +29,20 @@
 // SUBMISSION DEADLINE
 // The deadline for submissions is September 12, 2023, at 11:59 PM WAT. Late submissions will not be entertained.
 // NB: If you encounter any issues or have questions regarding the submission process, please message any of the frontend mentors.
+
+const day = document.querySelector("[data-testid='currentDayOfTheWeek']");
+
+const time = document.querySelector('[data-testid="currentUTCTime"');
+
+function getCurrentDate() {
+  let today = new Date();
+  let todayDate = today.toLocaleString("en-us", { weekday: "long" });
+  let utcTime = today.getTime();
+
+  day.textContent = todayDate;
+  time.textContent = utcTime;
+}
+
+getCurrentDate(); // Updates the date and time on initial render
+
+setInterval(getCurrentDate, 1000); // keep calling the function (to update the date and time) every 1000millisecond which is equivalent to 1 second
